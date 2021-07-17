dfa7d02c91153ffb2d8f132977facfb7b0a63150
Alpine999 <84825854+Alpine999@users.noreply.github.com>
2021-07-16 23:50:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var base
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+  base = new base
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+
+}
